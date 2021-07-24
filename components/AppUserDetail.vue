838113b35a0eef37a2cbe80e4d18a6970297b923
kaito_ohishi <kaito990626@yahoo.o.jp+ohishikaito@users.noreply.github.com>
2021-07-25 05:07:18 +0900

    feature/AppUserDetailを更新

--- components/AppUserDetail.vue
+++ components/AppUserDetail.vue
@@ -1,17 +1,29 @@
 <template>
-  <v-card width="600">
-    <v-card-title>
-      <h2 class="font-weight-bold">
-        {{ user.uid }}
-      </h2>
-    </v-card-title>
-    <v-card-subtitle>
-      <p class="font-weight-regular text--secondary">
-        {{ user.email }}
-        {{ user.fullName }}
-      </p>
-    </v-card-subtitle>
-  </v-card>
+  <div class="user-detail d-flex">
+    <div class="left-card d-flex flex-column mr-5">
+      <v-card class="mb-3">
+        <nuxt-link :to="`users/${user.uid}`">
+          <v-img
+            src="https://4.bp.blogspot.com/-bTipX3Vmpts/Wn1ZgUbOHXI/AAAAAAABKM4/b31Jvq8aWssiswuiO19BAJmmAC5WAzXwACLcBGAs/s800/character_boy_normal.png"
+          />
+        </nuxt-link>
+      </v-card>
+      <v-card class="pa-7">
+        <h2>{{ user.fullName }}</h2>
+        <p>{{ 'N' }}歳 東京</p>
+        <p class="pt-2">{{ 'ここに一言コメントが入ります' }}</p>
+      </v-card>
+    </div>
+    <div class="right-card">
+      <v-card class="pa-10 mb-3">
+        <h3 class="pb-3">自己紹介文</h3>
+        {{ 'ここに自己紹介文が入る予定です' }}
+      </v-card>
+      <v-card class="pa-10">
+        <h3>プロフィール</h3>
+      </v-card>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -27,4 +39,18 @@ export default defineComponent({
 })
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.user-detail {
+  .v-card {
+    border-radius: 10px;
+  }
+}
+.left-card {
+  width: 100%;
+  max-width: 320px;
+}
+.right-card {
+  width: 100%;
+  max-width: 375px;
+}
+</style>
